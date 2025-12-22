624e3ee175fcb4e913cc2b740fa4d5c6f7dffc3c
Trials Tricks <trialstricks@gmail.com>
2025-12-22 09:12:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1841160812-25f71",
+  "appId": "1:86059021036:web:01aadc079085ad2ce36bc9",
+  "apiKey": "AIzaSyDbVk-vG7mwXHucc5bKTL_j1xY4wGUpNYc",
+  "authDomain": "studio-1841160812-25f71.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "86059021036"
+};
