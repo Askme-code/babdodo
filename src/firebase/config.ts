ccaa393158448f4119cd034637938535d577d5d1
Trials Tricks <trialstricks@gmail.com>
2025-12-22 09:40:20 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-1841160812-25f71",
-  "appId": "1:86059021036:web:01aadc079085ad2ce36bc9",
-  "apiKey": "AIzaSyDbVk-vG7mwXHucc5bKTL_j1xY4wGUpNYc",
-  "authDomain": "studio-1841160812-25f71.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "86059021036"
+  apiKey: "AIzaSyBZl0S32WChpi_Csj7SFNO3tinKgp6qLTo",
+  authDomain: "babdodo-1549f.firebaseapp.com",
+  projectId: "babdodo-1549f",
+  storageBucket: "babdodo-1549f.firebasestorage.app",
+  messagingSenderId: "859736820042",
+  appId: "1:859736820042:web:b368b15c9ca102e2d9d9b3",
+  measurementId: "G-4CXSPFYGL0"
 };
