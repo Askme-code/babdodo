29aac27e41e886f7cd55f348eb21df60ea78fcac
Trials Tricks <trialstricks@gmail.com>
2025-12-27 22:44:17 +0000

    Try fixing this error: `Console Error: Invalid src prop (https://youtube

--- src/components/MediaRenderer.tsx
+++ src/components/MediaRenderer.tsx
@@ -17,7 +17,7 @@ interface MediaRendererProps {
 
 const getYouTubeId = (url: string): string | null => {
   if (!url) return null;
-  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|shorts\/|watch\?v=|&v=)([^#&?]*).*/;
   const match = url.match(regExp);
   return (match && match[2].length === 11) ? match[2] : null;
 };
