53a4b17a86960c2f362fafc2a776d76002698be8
Trials Tricks <trialstricks@gmail.com>
2025-12-28 00:30:36 +0000

    im also using image from https://www.pexels.com/search/ruaha%20national%

--- next.config.ts
+++ next.config.ts
@@ -36,8 +36,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-        ,
+        protocol: 'https',
         hostname: 'i.ytimg.com',
         port: '',
         pathname: '/**',
